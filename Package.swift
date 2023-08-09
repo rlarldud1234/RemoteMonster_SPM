816a7c88f4a6f4ac77d69b41e883d92c3e5b4a8f
김기영 <rlarldud6482@naver.com>
2023-08-09 09:29:00 +0900

    package path 지정

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
 	name: "RemoteMonster",
 	platforms: [
-		.iOS(.v14)
+		.iOS(.v13)
 	],
 	products: [
 		.library(
@@ -25,13 +25,11 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "RemoteMonster",
-			url: "https://github.com/yim2627/RemoteMonster_SPM/releases/download/v2.7.16/RemoteMonster.framework.zip",
-			checksum: "a0cbf2f272b69f52d4d7c7f872a07ddf9f3cd6d6c2362e2039b059ee5e59a961"
-		),
+            path: "RemoteMonster.xcframework"
+        ),
 		.binaryTarget(
 			name: "WebRTC",
-			url: "https://github.com/yim2627/RemoteMonster_SPM/releases/download/v2.7.16/WebRTC.framework.zip",
-			checksum: "e55cc77382e51a7c2c631d1657a04b84c1ef58d6246f9fadd93d74a5a391dc45"
-		)
+            path: "WebRTC.xcframework"
+        )
 	]
 )
